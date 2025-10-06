5b89c1ff06eb313ccda8767bcbafaf947e7c31b7
prosperouspi <huntercmcgraw@gmail.com>
2025-10-05 23:24:02 -0400

    Update oneBadApple.c

--- oneBadApple.c
+++ oneBadApple.c
@@ -12,7 +12,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void next_node(int node_id, int fd, int num_nodes, int fd_final);
+void next_node(int node_id, char *fd, int num_nodes, char *fd_final);
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +20,8 @@ int main(int argc, char *argv[])
     int pipe_creation_result;
     pid_t pid, child;
     int status;
-    int input;
-    int output = 3;
+    char input[128];
+    char output[128] = "Apple";
     
     int k = atoi(argv[1]);
     pipe_creation_result = pipe(fd);
@@ -32,10 +32,10 @@ int main(int argc, char *argv[])
     if (pid == 0) {
         next_node(1, fd[0], k, fd_final[1]);
     } else { 
-        write(fd[1], &output, sizeof(int));
-        printf("Node 0 wrote [%d]\n",  output);
-        read(fd_final[0], &input, sizeof(int));
-        printf("Node 0 received [%d]\n", input);
+        write(fd[1], output, sizeof(output));
+        printf("Node 0 wrote [%s]\n",  output);
+        read(fd_final[0], input, sizeof(input));
+        printf("Node 0 received [%s]\n", input);
     }
     
     return 0;
@@ -46,9 +46,11 @@ void next_node(int node_id, int fd, int num_nodes, int fd_final) {
         exit(0);
     }
     int input;
-    int output = rand() % 5;
-    read(fd, &input, sizeof(int));
-    printf("Node %d received [%d]\n", node_id, input);
+    read(fd, input, sizeof(input));
+    printf("Node %d received [%s]\n", node_id, input);
+
+    char output[128] = input;
+
     int fd_next[2];
     int pipe_creation_result_next = pipe(fd_next);
     pid_t pid_next = fork();
@@ -56,10 +58,10 @@ void next_node(int node_id, int fd, int num_nodes, int fd_final) {
         next_node(node_id + 1, fd_next[0], num_nodes, fd_final);
     } else {
         if (node_id == num_nodes - 1) { 
-           write(fd_final, &output, sizeof(int));
+           write(fd_final, output, sizeof(output));
         } else {
-           write(fd_next[1], &output, sizeof(int));
+           write(fd_next[1], output, sizeof(output));
         }
-        printf("Node %d wrote [%d]\n", node_id, output);
+        printf("Node %d wrote [%s]\n", node_id, output);
     }
 }
